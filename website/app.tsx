4d5f8c9cb012efb4f77cb9e57f9f209e2b23af5b
Jacques <jverre@gmail.com>
2025-09-14 22:14:17 +0100

    User auth

--- website/app.tsx
+++ website/app.tsx
@@ -4,5 +4,10 @@ import { createRouter } from './src/router'
 const router = createRouter()
 
 export default function App() {
-  return <StartClient router={router} />
+  return (
+    <StartClient
+      router={router}
+      devtools={process.env.NODE_ENV === 'development'}
+    />
+  )
 }
